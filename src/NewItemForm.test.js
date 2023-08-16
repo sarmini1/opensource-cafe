cd1b9a6be3c04fe3da35b64353580d2a61dd2b56
Spencer Armini <spencerarmini@gmail.com>
2023-08-15 19:17:40 -0700

    fixed failing form tests
    needed to wrap a memory router component around the new item form when rendering in tests

--- src/NewItemForm.test.js
+++ src/NewItemForm.test.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { MemoryRouter } from "react-router-dom";
+
 import NewItemForm from "./NewItemForm";
 import { addItem } from "./App";
 
 it('mounts without crashing', function () {
-  render(<NewItemForm addItem={addItem} />)
+  render(<MemoryRouter><NewItemForm addItem={addItem} /></MemoryRouter>);
 });
 
 it("matches snapshot", function () {
-  const { debug, container } = render(<NewItemForm addItem={addItem} />)
+  const { debug, container } = render(
+    <MemoryRouter><NewItemForm addItem={addItem} /></MemoryRouter>
+  );
 
   debug(container);
   expect(container).toMatchSnapshot();
 });
 
 it("displays a form to add a new item", function () {
-  const { debug, container } = render(<NewItemForm addItem={addItem} />);
+  const { debug, container } = render(
+    <MemoryRouter><NewItemForm addItem={addItem} /></MemoryRouter>
+  );
 
   debug(container);
   const form = container.querySelector(".NewItemForm-form");
